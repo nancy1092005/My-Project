75b43c19a9e527d9dfb8437c906736cdf551e622
nancy1092005 <nancy1092005@gmail.com>
2025-07-29 15:57:27 +0530

    Update Find.jsx

--- clientserver-app/src/Component/Find.jsx
+++ clientserver-app/src/Component/Find.jsx
@@ -11,7 +11,7 @@ export function Find(){
         e.preventDefault();
         try
         {
-           const response = await axios.get (`http://localhost:3002/api/employees/${id}`);
+           const response = await axios.get (`https://my-project-roxw.onrender.com/api/employees/${id}`);
            setEmployee(response.data);
 
         }
